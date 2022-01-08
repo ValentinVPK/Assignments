0d6a74075962eb9a032a90df5c7ddb7367addf66
ValentinVPK <valentinvpk@abv.bg>
2022-01-08 20:49:02 +0200

    Added another assignment

---
+++ CheckBrackets/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheckBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution("abc()")); // true
+            Console.WriteLine(Solution("abc(})adada")); // false
+            Console.WriteLine(Solution("abc{(})adada")); // false
+            Console.WriteLine(Solution("[abc()]adada")); // true
+        }
+
+        public static bool Solution(string input)
+        {
+            Stack<char> openingBrackets = new Stack<char>();
+
+            bool isCorrect = true;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '{' || input[i] == '(' || input[i] == '[')
+                {
+                    openingBrackets.Push(input[i]);
+                    continue;
+                }
+                else if (input[i] != '}' && input[i] != ')' && input[i] != ']') continue;
+
+
+                if (openingBrackets.Count > 0)
+                {
+                    if (input[i] == '}')
+                    {
+                        if (openingBrackets.Peek() == '{') openingBrackets.Pop();
+                        else
+                        {
+                            isCorrect = false;
+                            break;
+                        }
+                    }
+
+                    if (input[i] == ')')
+                    {
+                        if (openingBrackets.Peek() == '(') openingBrackets.Pop();
+                        else
+                        {
+                            isCorrect = false;
+                            break;
+                        }
+                    }
+
+                    if (input[i] == ']')
+                    {
+                        if (openingBrackets.Peek() == '[') openingBrackets.Pop();
+                        else
+                        {
+                            isCorrect = false;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    isCorrect = false;
+                    break;
+                }
+            }
+
+            if (openingBrackets.Count > 0) isCorrect = false;
+
+            return isCorrect;
+        }
+    }
+}
---
+++ CheckBrackets/README.md
@@ -0,0 +1,5 @@
+﻿**Check Brackets** 
+
+Write method bool Solution(string input) which parses a string and checks if all brackets are closed correctly.  
+
+Allowed bracket types: {}, (), []. 
